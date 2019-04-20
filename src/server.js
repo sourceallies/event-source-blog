49b34ff92f581733077552237b5b43176f904de4
Paul Rowe <prowe@sourceallies.com>
2019-04-19 21:32:34 -0500

    working on schema

--- src/server.js
+++ src/server.js
@@ -8,9 +8,22 @@ function logRequestEvent(_request, {tags, data}) {
     console.log(tags, data);
 }
 
+// WIP
+// async function failAction(request, h, err) {
+//     return h.response(err.details || err)
+//         .code(400)
+//         .takeover;
+// }
+
 async function init() {
     const server = new Server({
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        routes: {
+            cors: true,
+            validate: {
+                // failAction
+            }
+        }
     });
     server.events.on('request', logRequestEvent);
     await setupMongo(server);
--- src/shipments/events/create/createEventSchema.js
+++ src/shipments/events/create/createEventSchema.js
@@ -1,5 +1,22 @@
 const Joi = require('joi');
 
 module.exports = Joi.object({
-
+    shipFrom: Joi.object({
+        name: Joi.string().required(),
+        line1: Joi.string().required(),
+        line2: Joi.string(),
+        city: Joi.string().required(),
+        state: Joi.string().length(2).required(),
+        zip: Joi.string().length(5)
+    }).required(),
+    shipTo: Joi.object({
+        name: Joi.string().required(),
+        line1: Joi.string().required(),
+        line2: Joi.string(),
+        city: Joi.string().required(),
+        state: Joi.string().length(2).required(),
+        zip: Joi.string().length(5)
+    }).required(),
+    weightInPounds: Joi.number()
+        .min(1)
 });
