572e4c0d35b2a9e1444325cb865c0a73ff2b6470
Paul Rowe <prowe@sourceallies.com>
2019-04-20 20:19:55 -0500

    cleanup

--- src/shipments/events/create/createEventSchema.js
+++ src/shipments/events/create/createEventSchema.js
@@ -2,21 +2,24 @@ const Joi = require('joi');
 
 module.exports = Joi.object({
     shipFrom: Joi.object({
-        name: Joi.string().required(),
-        line1: Joi.string().required(),
-        line2: Joi.string(),
-        city: Joi.string().required(),
-        state: Joi.string().length(2).required(),
+        name: Joi.string(),
+        line1: Joi.string(),
+        line2: Joi.string().optional(),
+        city: Joi.string(),
+        state: Joi.string().length(2),
         zip: Joi.string().length(5)
-    }).required(),
+    }),
     shipTo: Joi.object({
-        name: Joi.string().required(),
-        line1: Joi.string().required(),
-        line2: Joi.string(),
-        city: Joi.string().required(),
-        state: Joi.string().length(2).required(),
+        name: Joi.string(),
+        line1: Joi.string(),
+        line2: Joi.string().optional(),
+        city: Joi.string(),
+        state: Joi.string().length(2),
         zip: Joi.string().length(5)
-    }).required(),
+    }),
     weightInPounds: Joi.number()
         .min(1)
+}).options({
+    abortEarly: false,
+    presence: 'required'
 });
