4ca24de882c6e8434fdb8a9102be03bd81ac6b3b
Paul Rowe <prowe@sourceallies.com>
2019-05-17 20:52:11 -0500

    deliver event

---
+++ src/shipments/events/deliver/deliverEventReducer.js
@@ -0,0 +1,27 @@
+
+const Joi = require('joi');
+const IllegalShipmentStateError = require('../IllegalShipmentStateError');
+
+const validShipmentSchema = Joi.object({
+    status: Joi.string().valid('Shipped')
+})
+    .required()
+    .options({
+        allowUnknown: true
+    });
+
+function validateShipment(shipment) {
+    const {error} = validShipmentSchema.validate(shipment);
+    if (error) {
+        throw new IllegalShipmentStateError(error.message);
+    }
+}
+
+module.exports = function deliverEventReducer(shipment) {
+    validateShipment(shipment);
+
+    return {
+        ...shipment,
+        status: 'Delivered'
+    };
+};
