011affb174db1b53809a1b30f564ae3aee3392f6
Paul Rowe <prowe@sourceallies.com>
2019-07-01 13:05:49 -0500

    cleanup

--- src/accounts/events/publish.js
+++ src/accounts/events/publish.js
@@ -1,6 +1,8 @@
 
 const {producer} = require('../../configuredKafka');
 
+//TODO: move me
+
 module.exports = async function publishEvent(event) {
     await producer.send({
         topic: 'account-events',
